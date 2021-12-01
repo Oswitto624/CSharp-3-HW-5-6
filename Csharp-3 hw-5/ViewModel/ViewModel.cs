6fcf50c8c4d05714c37a3742c3d26fa2b44f8fba
Oswitto624 <dzirtsmd@mail.ru>
2021-12-02 01:05:08 +0300

    Обновление 1. Допилены все элементы WPF приложения, кроме многопоточности. Многопоточность реализована крайне криво, работает коряво, буду ещё переделывать (сейчас работает через Thread)

--- Csharp-3 hw-5/ViewModel/ViewModel.cs
+++ Csharp-3 hw-5/ViewModel/ViewModel.cs
@@ -10,7 +10,7 @@ namespace Csharp_3_hw_5.ViewModel
     class ViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
-        
+
         public ICommand StartCalc
         {
             get
@@ -109,56 +109,185 @@ namespace Csharp_3_hw_5.ViewModel
             }
         }
 
+        Model.EvenNum evenNum = new Model.EvenNum();
+        int evenCount = 0;
+        public int EvenCount
+        {
+            get
+            {
+                return evenNum.Count;
+            }
+            set
+            {
+                if (evenCount != evenNum.Count)
+                {
+                    evenCount = evenNum.Count;
+                    PropertyChanged.Invoke(this, new PropertyChangedEventArgs("evenCount"));
+                    Debug.WriteLine("Work for EvenNum. Количество чисел: " + evenCount);
+                }
+            }
+        }
+        string evenCountTime = "none";
+        public string EvenCountTime
+        {
+            get
+            {
+                return evenCountTime;
+            }
+            set
+            {
+                if (evenCountTime != evenNum.CalcTime)
+                {
+                    evenCountTime = evenNum.CalcTime;
+                    PropertyChanged.Invoke(this, new PropertyChangedEventArgs("evenCountTime"));
+                    Debug.WriteLine("Work for EvenNum. Время выполнения: " + evenCountTime);
+                }
+                
+            }
+        }
+
+        Model.MultipleOf3and5 multipleNum = new Model.MultipleOf3and5();
+        int multipleCount = 0;
+        public int MultipleCount
+        {
+            get
+            {
+                return multipleCount;
+            }
+            set
+            {
+                if (multipleCount != multipleNum.Count)
+                {
+                    multipleCount = multipleNum.Count;
+                    PropertyChanged.Invoke(this, new PropertyChangedEventArgs("multipleCount"));
+                    Debug.WriteLine("Work for multipleCount. Количество чисел: " + multipleCount);
+                }
+            }
+        }
+        string multipleCountTime = "none";
+        public string MultipleCountTime
+        {
+            get
+            {
+                return multipleCountTime;
+            }
+            set
+            {
+                if (multipleCountTime != simpleNum.CalcTime)
+                {
+                    multipleCountTime = multipleNum.CalcTime;
+                    PropertyChanged.Invoke(this, new PropertyChangedEventArgs("multipleCountTime"));
+                    Debug.WriteLine("Work for multipleCountTime. Время выполнения: " + multipleCountTime);
+                }
+            }
+        }
+
+        Model.SimpleNum simpleNum = new Model.SimpleNum();
+        int simpleCount = 0;
+        public int SimpleCount
+        {
+            get
+            {
+                return simpleCount;
+            }
+            set
+            {
+                if (simpleCount != simpleNum.Count)
+                {
+                    simpleCount = simpleNum.Count;
+                    PropertyChanged.Invoke(this, new PropertyChangedEventArgs("SimpleCount"));
+                    Debug.WriteLine("Work for SimpleCount. Количество чисел: " + simpleCount);
+                }
+            }
+        }
+        string simpleCountTime = "none";
+        public string SimpleCountTime
+        {
+            get
+            {
+                return simpleCountTime;
+            }
+            set
+            {
+                if (simpleCountTime != simpleNum.CalcTime)
+                {
+                    simpleCountTime = simpleNum.CalcTime;
+                    PropertyChanged.Invoke(this, new PropertyChangedEventArgs("simpleCountTime"));
+                    Debug.WriteLine("Work for simpleCountTime. Время выполнения: " + simpleCountTime);
+                }
+            }
+        }
+        
+        Model.NumPow2 numPow = new Model.NumPow2();
+        int numPowCount = 0;
+        public int NumPowCount
+        {
+            get
+            {
+                return numPowCount;
+            }
+            set
+            {
+                if (numPowCount != numPow.Count)
+                {
+                    numPowCount = numPow.Count;
+                    PropertyChanged.Invoke(this, new PropertyChangedEventArgs("numPowCount"));
+                    Debug.WriteLine("Work for numPowCount. Количество чисел: " + numPowCount);
+                }
+            }
+        }
+        string numPowCountTime = "none";
+        public string NumPowCountTime
+        {
+            get
+            {
+                return numPowCountTime;
+            }
+            set
+            {
+                if (numPowCountTime != numPow.CalcTime)
+                {
+                    numPowCountTime = numPow.CalcTime;
+                    PropertyChanged.Invoke(this, new PropertyChangedEventArgs("numPowCountTime"));
+                    Debug.WriteLine("Work for numPowCountTime. Время выполнения: " + numPowCountTime);
+                }
+            }
+        }
+
         private void _StartCalc(object obj)
         {
-            int[] a = Model.Logic.ReadFile(FilenameText);
+            int[] a = Model.MainLogic.ReadFile(FilenameText);
 
             if (_chkEvenNum)
             {
-                Stopwatch stopwatch1 = new Stopwatch();
-                stopwatch1.Start();
-                int b = Model.Logic.EvenNum(a);
-                stopwatch1.Stop();
-                TimeSpan ts1 = stopwatch1.Elapsed; 
-                Debug.WriteLine("Work to EvenNum: " + b);
-                Debug.WriteLine("Elapsed Time: " + ts1.ToString());
-
+                evenNum._Calc(a);
+                EvenCount = evenNum.Count;
+                EvenCountTime = evenNum.CalcTime;
+                Debug.WriteLine("Work to EvenNum complete.");
             }
 
             if (_chkMultipleOf3and5)
             {
-                Stopwatch stopwatch2 = new Stopwatch();
-                stopwatch2.Start();
-                int c = Model.Logic.MultipleOf3and5(a);
-                stopwatch2.Stop();
-                TimeSpan ts2 = stopwatch2.Elapsed;
-                Debug.WriteLine("Work to Multiple3and5: " + c);
-                Debug.WriteLine("Elapsed Time: " + ts2.ToString());
-
+                multipleNum._Calc(a);
+                MultipleCount = multipleNum.Count;
+                MultipleCountTime = multipleNum.CalcTime;
+                Debug.WriteLine("Work to Multiple3and5 complete.");
             }
 
             if (_chkSimpleNum)
             {
-                Stopwatch stopwatch3 = new Stopwatch();
-                stopwatch3.Start();
-                int d = Model.Logic.SimpleNum(a);
-                stopwatch3.Stop();
-                TimeSpan ts3 = stopwatch3.Elapsed;
-                Debug.WriteLine("Work to SimpleNum: " + d);
-                Debug.WriteLine("Elapsed Time: " + ts3.ToString());
-
+                simpleNum._Calc(a);
+                SimpleCount = simpleNum.Count;
+                SimpleCountTime = simpleNum.CalcTime;
+                Debug.WriteLine("Work to SimpleNum complete.");
             }
 
             if (_chkNumPow2)
             {
-                Stopwatch stopwatch4 = new Stopwatch();
-                stopwatch4.Start();
-                int e = Model.Logic.NumPow2(a);
-                stopwatch4.Stop();
-                TimeSpan ts4 = stopwatch4.Elapsed; 
-                Debug.WriteLine("Work to NumPow2: " + e);
-                Debug.WriteLine("Elapsed Time: " + ts4.ToString());
-
+                numPow._Calc(a);
+                NumPowCount = numPow.Count;
+                NumPowCountTime = numPow.CalcTime;
+                Debug.WriteLine("Work to NumPow2 complete.");
             }
 
             Debug.WriteLine("All works has been ended.");
